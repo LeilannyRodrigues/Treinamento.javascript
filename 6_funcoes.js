f46d80d38e8a37ac337bc24987fc0eac09292be4
Leilanny Rodrigues <leilannyrodrigues@gmail.com>
2025-05-23 14:43:06 -0300

    Create 6_funcoes.js

---
+++ 6_funcoes.js
@@ -0,0 +1,22 @@
+// function goodMorning(name){
+   // console.log("Bom dia!");
+ //  return `Bom dia ${name}!`
+//}
+
+// console.log(goodMorning ("Leilanny"))
+
+/* const goodMorning = (name) => {
+  return `Bom dia ${name}`
+}
+
+console.log(goodMorning("Leilanny")) */
+
+const adicao = (x,y) => x+y;
+
+const subtracao = (x,y) => x-y;
+
+const multiplicação = (x,y) => x*y;
+
+const divisao = (x,y) => x/y;
+
+console.log(`O resultado: ${multiplicação(12,6)}`);
