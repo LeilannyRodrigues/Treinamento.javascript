55c22aecc90afa4bdd3eaebee27f93b202afe59c
Leilanny Rodrigues <leilannyrodrigues@gmail.com>
2025-05-23 14:40:12 -0300

    Create 2_variaveis.js

---
+++ 2_variaveis.js
@@ -0,0 +1,31 @@
+const name = "Leilanny"
+
+const lastName = "Rodrigues"
+
+const idade = 25
+
+const valor1 = 2.10
+
+const valor2 = 3.20
+
+const frutas = ["Banana","Maçã","Mamão",20]
+
+const usuario = {
+    nome: "Leilanny",
+    lastName: "Rodrigues",
+    idade:25,
+}
+
+const vazio = null
+
+console.log(typeof name)
+
+// console.log(usuario.lastName)
+
+// console.log(frutas[1])
+
+// const isLogin = false
+
+/*console.log(`O valor total é ${valor1 + valor2}`)
+
+console.log (`Meu nome é ${name} ${lastName} e minha idade é ${idade}`) */
