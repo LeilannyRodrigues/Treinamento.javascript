14b214c3ceb85fb79a5405554095c2831258ced3
Leilanny Rodrigues <leilannyrodrigues@gmail.com>
2025-05-23 14:46:08 -0300

    Create 11_escopo.js

---
+++ 11_escopo.js
@@ -0,0 +1,19 @@
+// let minhaVariavelGlobal = "Eu sou uma variável global"
+
+// function minhaFuncao(){
+// console.log(minhaVariavelGlobal)
+// }
+
+// minhaFuncao()
+
+/* function escopo(){
+    let minhaVariavelLocal = "Eu sou uma variável local"
+    console.log(minhaVariavelLocal)
+}
+
+escopo() */
+
+if(true){
+    let minhaVariavelLocal = "variável local"
+    console.log(minhaVariavelLocal)
+}
